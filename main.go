b956ac04bd6227a2328c48a3ddfb43021736b265
Jorge Quilcate Otoya <quilcate.jorge@gmail.com>
2018-05-28 21:37:16 +0200

    move to package kafka

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
   "github.com/hashicorp/terraform/plugin"
-  "github.com/hashicorp/terraform/terraform"
+  "github.com/sysco-middleware/terraform-provider-kafka/kafka"
 )
 
 func main() {
   plugin.Serve(&plugin.ServeOpts{
-    ProviderFunc: func() terraform.ResourceProvider {
-      return Provider()
-    },
-  })
+    ProviderFunc: kafka.Provider})
 }
