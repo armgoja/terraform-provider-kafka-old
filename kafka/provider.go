399c71d20bc1215bedb9e04ef59b28c0415c3661
Jorge Quilcate Otoya <quilcate.jorge@gmail.com>
2018-05-28 21:41:46 +0200

    fix deps

--- kafka/provider.go
+++ kafka/provider.go
@@ -2,9 +2,10 @@ package kafka
 
 import (
   "github.com/hashicorp/terraform/helper/schema"
+  "github.com/hashicorp/terraform/terraform"
 )
 
-func Provider() *schema.Provider {
+func Provider() terraform.ResourceProvider {
   return &schema.Provider{
     ResourcesMap: map[string]*schema.Resource{
       "kafka_topic": resourceKafkaTopic(),
